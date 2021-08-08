971c0b94e9f91d9008aa822cbad3c1ce63f8b416
Kellie Werrell <kelliewerrell@gmail.com>
2021-08-08 10:57:50 -0700

    added fetchWeather funtion code * created apiURL and weathericon

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -4,9 +4,36 @@ var weather = $("#currentWeather");
 var forecast = $("#weatherForecast");
 var cities;
 
-function fetchWeather() {
-
+if (localStorage.getItem("localWeatherSearches")) {
+    cities = JSON.parse(localStorage.getItem("localWeatherSearches"));
+    writeSearchHistory(cities);
+} else {
+    cities = [];
 };
+
+function fetchWeather(cityName) {
+    let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&APPID=${apiKey}`;
+
+    $.get(apiURL).then(function (response) {
+        let currTime = new Date(response.dt * 1000);
+        let weatherIcon = `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`;
+
+        weather.html(`
+        <h2>${response.name}, 
+        ${response.sys.country}
+        (${currTime.getMonth() + 1}/${currTime.getDate()}/${currTime.getFullYear()})
+        <img src=${weatherIcon} height="70px"></h2>
+        <p>Temperature: ${response.main.temp} &#176;F</p>
+        <p>Humidity: ${response.main.humidity}%</p>
+        <p>Wind Speed: ${response.wind.speed} m/s</p>
+        `,
+
+            fetchUVIndex(response.coord))
+            historyBtn(response.name);
+    })
+};
+
+
 
 function fetchForecast() {
 
@@ -29,14 +56,14 @@ function searchHistory() {
 fetchWeather("Scottsdale");
 fetchForecast("Scottsdale");
 
-$("#submitCity").click(function() {
+$("#submitCity").click(function () {
     event.preventDefault();
     let cityName = $("#cityInput").val();
     fetchWeather(cityName);
     fetchForecast(cityName);
 });
 
-$("#previousSearch").click(function() {
+$("#previousSearch").click(function () {
     let cityName = event.target.value;
     fetchWeather(cityName);
     fetchForecast(cityName);
