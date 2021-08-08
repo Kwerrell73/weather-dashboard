8bf267f22c752938b6a41a9b0b170ca688c3ebcf
Kellie Werrell <kelliewerrell@gmail.com>
2021-08-08 10:29:55 -0700

    added globabl vars for weather forecast and cities * added setup for 5 functions must complete

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,1 +1,25 @@
-const apiKey = "";
+const apiKey = "823d12533533a33a3f03e7bef61cae69";
+
+var weather = $("#currentWeather");
+var forecast = $("#weatherForecast");
+var cities;
+
+function fetchWeather() {
+
+};
+
+function fetchForecast() {
+
+};
+
+function fetchUVIndex() {
+
+};
+
+function historyBtn() {
+
+};
+
+function searchHistory() {
+
+};
