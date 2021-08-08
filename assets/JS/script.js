6c5852568268fa21a2621418e5ee02e05882b896
Kellie Werrell <kelliewerrell@gmail.com>
2021-08-08 11:09:09 -0700

    added notation and tested fetchWeather function * adding apcong between temp, wind, and humidity

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -15,17 +15,23 @@ function fetchWeather(cityName) {
     let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&APPID=${apiKey}`;
 
     $.get(apiURL).then(function (response) {
-        let currTime = new Date(response.dt * 1000);
-        let weatherIcon = `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`;
+        let time = new Date(response.dt * 1000);
+        let weatherMapIcon = `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`;
 
-        weather.html(`
+        weather.html
+        //return city name, country, and month, date and year
+        //then return temp, wind speed and humidity
+        (`
         <h2>${response.name}, 
         ${response.sys.country}
-        (${currTime.getMonth() + 1}/${currTime.getDate()}/${currTime.getFullYear()})
-        <img src=${weatherIcon} height="70px"></h2>
-        <p>Temperature: ${response.main.temp} &#176;F</p>
-        <p>Humidity: ${response.main.humidity}%</p>
-        <p>Wind Speed: ${response.wind.speed} m/s</p>
+        (${time.getMonth() + 1}/${time.getDate()}/${time.getFullYear()})
+        <img src=${weatherMapIcon} height="70px"></h2>
+
+        <p>Temperature: ${response.main.temp} &#176;F,<br>
+        <br>
+        Wind Speed: ${response.wind.speed} m/s,<br>
+        <br>
+        Humidity: ${response.main.humidity}%</p>
         `,
 
             fetchUVIndex(response.coord))
